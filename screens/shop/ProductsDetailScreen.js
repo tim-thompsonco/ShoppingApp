f63553cb796857d0abe3ec1fb528cacb70bdaeef
Tim Thompson <scubatim84@gmail.com>
2020-09-13 10:07:21 -0600

    Build out ProductsDetailScreen and add styling to screen

--- screens/shop/ProductsDetailScreen.js
+++ screens/shop/ProductsDetailScreen.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import Colors from '../../constants/Colors';
+
 const ProductsDetailScreen = (props) => {
   const productId = props.navigation.getParam('productId');
 
@@ -17,9 +19,15 @@ const ProductsDetailScreen = (props) => {
   );
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title='Add to Cart' onPress={() => {}} />
+      </View>
+
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -29,6 +37,26 @@ ProductsDetailScreen.navigationOptions = (navData) => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20,
+  },
+});
 
 export default ProductsDetailScreen;
