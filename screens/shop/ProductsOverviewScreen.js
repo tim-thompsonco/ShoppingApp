6f469677f4f410b69abf142b827d7872882f239d
Tim Thompson <scubatim84@gmail.com>
2020-09-13 08:43:47 -0600

    Add ProductItem component, change ProductsOverviewScreen to render flat list of ProductItem components

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { FlatList, Text } from 'react-native';
 
+import ProductItem from '../../components/shop/ProductItem';
+
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
 
   return (
     <FlatList
       data={products}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          title={itemData.item.title}
+          imageUrl={itemData.item.imageUrl}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
