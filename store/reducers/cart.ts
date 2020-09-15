f96a7c2593839465d98f7d4c37a9781b8839c920
Tim Thompson <scubatim84@gmail.com>
2020-09-15 13:19:27 -0600

    Add functionality so that cart is cleared after checkout button is clicked

--- store/reducers/cart.ts
+++ store/reducers/cart.ts
@@ -1,4 +1,5 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
+import { ADD_ORDER } from '../actions/orders';
 import CartItem from '../../models/cart-item';
 import Product from '../../models/product';
 
@@ -60,6 +61,8 @@ export default (state = initialState, action: any) => {
         items: updatedCartItems,
         totalAmount: state.totalAmount - selectedCartItem.productPrice,
       };
+    case ADD_ORDER:
+      return initialState;
     default:
       return state;
   }
